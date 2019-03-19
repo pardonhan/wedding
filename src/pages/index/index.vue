feaed6a327b25d00496e4e54487a73128699c6cf
huangbin <18358274764@163.com>
2019-03-19 15:32:28 +0800

    清除无关的css代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -191,17 +191,4 @@ export default {
         width 520rpx
   #myAudio
     display none
-  .loading-container
-    position fixed
-    width 100%
-    height 100%
-    top 0
-    bottom 0
-    left 0
-    right 0
-    background #fff
-    margin auto
-    display flex
-    justify-content center
-    align-items center
 </style>
